0279e7badef522b04f3f38c4d3c33e0ea83795ab
unknown <milosz20031207@gmail.com>
2022-07-22 22:55:58 +0200

    feature: added title card with animations

--- src/Components/TitleCard/Title.tsx
+++ src/Components/TitleCard/Title.tsx
@@ -1,16 +1,65 @@
 import { FC } from "react";
 import { styles } from "./TitleCardStyles";
+import { motion } from "framer-motion";
 
 const Title: FC = () => {
 
     return (
         <div className={ styles.title_card_wrap }>
             <div className={ styles.title_card_upper }>
-                {"hey \nI'm milo"}
+                <motion.p 
+                    transition={ { 
+                            duration: 1,
+                            delay: .8,
+                            ease: [ 0.850, -0.015, 0.360, 0.980 ]
+                    } }
+                    initial={ { transform: 'translate( 0, -100% )' } }
+                    animate={ { transform: 'translate( 0, 0% )' } }
+                    exit={ { transform: 'translate( 0, 100% )' } }
+                >
+                    {"hey \nI'm milo"}
+                </motion.p>
             </div>
+            <motion.div 
+                className={ styles.title_card_line  } 
+                transition={ {
+                    duration: .8,
+                    ease: [ 0.460, 0.100, 0.000, 0.960 ]
+                } }
+                initial={ { maxWidth: 0 } }
+                animate={ { maxWidth: 'calc( 100% - 1rem - 14ch )' } }
+                exit={ { maxWidth: 0 } }
+            />
             <div className={ styles.title_card_bottom }>
-                {"I like \nmaking stuff"}
+                <motion.p
+                    transition={ { 
+                        duration: 1,
+                        delay: .8,
+                        ease: [ 0.850, -0.015, 0.360, 0.980 ]
+                    } }
+                    initial={ { transform: 'translate( 0, 100% )' } }
+                    animate={ { transform: 'translate( 0, 0% )' } }
+                    exit={ { transform: 'translate( 0, -100% )' } }    
+                >
+                    {"I like \nmaking stuff"}
+                </motion.p>
             </div>
+            <motion.div 
+                className={ styles.title_card_footer }  
+            >
+            <motion.p
+                transition={ { 
+                    duration: 1,
+                    delay: 1.8,
+                    ease: [ 0.850, -0.015, 0.360, 0.980 ]
+                } }
+                initial={ { transform: 'translate( 0, 100% )' } }
+                animate={ { transform: 'translate( 0, 0% )' } }
+                exit={ { transform: 'translate( 0, -100% )' } }  
+            >
+                {"scroll down ↓"}
+            </motion.p>
+            </motion.div>
         </div>
     )
 }
